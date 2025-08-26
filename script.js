6b0ca534b55dbcba9d7bb5404e20826bb4a3ff6e
micheal-troy <michaelkufre4@gmail.com>
2025-08-26 12:18:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for nav links (optional future upgrade)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
